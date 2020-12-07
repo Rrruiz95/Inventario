5f5bcef7de8b65664bee4402f3b5bbccadc05661
Ricardo Ruiz <rruiz_95@outlook.com>
2020-12-07 14:52:35 -0600

    Manejador de Vales terminado en BIZ

---
+++ Inventario.COMMON/Interfaces/IManejadorEmpleados.cs
@@ -0,0 +1,11 @@
+﻿using Inventario.COMMON.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventario.COMMON.Interfaces
+{
+    interface IManejadorEmpleados : IManejadorGenerico<Empleado>
+    {
+    }
+}
